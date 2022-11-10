c82003c6d6cc62da2e28e342845f3cf8d4b11a2c
roei naveh <roei.naveh@outlook.com>
2022-11-10 20:27:48 +0200

    Add project files.

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0,index;
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.RightArrow) x++;
+                if (Console.ReadKey().Key == ConsoleKey.LeftArrow)  x--;
+                if (Console.ReadKey().Key == ConsoleKey.UpArrow)  y--;
+                if (Console.ReadKey().Key == ConsoleKey.DownArrow)  y++;
+                Console.Clear();
+                for (index=1; index<= y;index++) Console.WriteLine();
+                for (index=1;index<=x;index++) Console.Write("  ");
+                Console.Write("<");
+            }
+        }
+    }
+}
